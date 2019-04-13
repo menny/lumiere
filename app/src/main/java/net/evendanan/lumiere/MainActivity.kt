eae473b62e76350298c79439726b8f4292edf991
Menny Even Danan <menny@evendanan.net>
2019-04-13 13:06:45 -0400

    Refactor sections recyclers for more generic code

--- app/src/main/java/net/evendanan/lumiere/MainActivity.kt
+++ app/src/main/java/net/evendanan/lumiere/MainActivity.kt
@@ -4,9 +4,12 @@ import android.app.Activity
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,29 +21,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var presenter: Presenter
+    private lateinit var loadingPlaceholder: Drawable
+    private lateinit var loadingError: Drawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { presenter.onFabClicked() }
         search_query.setOnEditorActionListener { _, actionId, _ ->
             when (actionId) {
-                EditorInfo.IME_ACTION_SEARCH -> { presenter.onFabClicked(); true }
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    presenter.onFabClicked(); true
+                }
                 else -> false
             }
         }
 
-        val placeholder = CircularProgressDrawable(this).apply {
+        loadingPlaceholder = CircularProgressDrawable(this).apply {
             centerRadius = resources.getDimension(R.dimen.loading_radius)
             strokeWidth = resources.getDimension(R.dimen.loading_stroke_wide)
             start()
         }
-        trending_list.adapter = MediaItemsAdapter(this, placeholder)
-        trending_list.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-        search_results_list.adapter = MediaItemsAdapter(this, placeholder)
-        search_results_list.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        loadingError = getDrawable(R.drawable.ic_error_loading)!!
+
+        root_list.adapter = SectionsAdapter(this, loadingPlaceholder, loadingError)
+        root_list.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 
         presenter = PresenterImpl(GiphyMediaProvider(getString(R.string.giphy_api_key)), UiPresenterBridge())
     }
@@ -60,64 +66,129 @@ class MainActivity : AppCompatActivity() {
         presenter.destroy()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     inner class UiPresenterBridge : PresenterUI {
+        override fun focusOnSection(providerType: ProviderType) {
+            root_list.scrollToPosition(root_list.adapter?.itemCount ?: 0 - 1)
+        }
+
+        override fun setItemsProviders(providers: List<ItemsProvider>) {
+            providers.forEach { Log.d("UiPresenterBridge", "provide: ${it.type}") }
+            (root_list.adapter as SectionsAdapter).setItemsProviders(providers)
+        }
+
         override fun setQueryBoxVisibility(visible: Boolean) {
             search_query.visibility = if (visible) View.VISIBLE else View.GONE
         }
 
         override fun getQueryBoxText() = search_query.text.toString()
-
-        override fun setSearchResults(entries: List<Media>) {
-            entries.forEach { Log.d("UiPresenterBridge", "search result: ${it.original}") }
-            search_results_card.visibility = View.VISIBLE
-            (search_results_list.adapter as MediaItemsAdapter).setItems(entries)
-        }
-
-        override fun setHistory(entries: List<Media>) {
-            entries.forEach { Log.d("UiPresenterBridge", "history: ${it.original}") }
-        }
-
-        override fun setFavorites(entries: List<Media>) {
-            entries.forEach { Log.d("UiPresenterBridge", "favorite: ${it.original}") }
-        }
-
-        override fun setTrending(entries: List<Media>) {
-            entries.forEach { Log.d("UiPresenterBridge", "trending: ${it.original}") }
-            (trending_list.adapter as MediaItemsAdapter).setItems(entries)
-        }
-
     }
 }
 
-private class MediaItemsAdapter(private val activity: Activity, private val placeholder: Drawable) :
+private class SectionsAdapter(
+    private val activity: Activity,
+    private val placeholder: Drawable, private val error: Drawable
+) : RecyclerView.Adapter<SectionViewHolder>() {
+    private val layoutInflater = LayoutInflater.from(activity)
+    private var itemsProviders = emptyList<ItemsProvider>()
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun getItemViewType(position: Int) = when (position) {
+        0 -> APP_SECTION_TYPE
+        itemsProviders.size + 1 -> SPACER_SECTION_TYPE
+        else -> MEDIA_SECTION_TYPE
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        when (viewType) {
+            APP_SECTION_TYPE -> SectionViewHolder.StaticSectionViewHolder(
+                layoutInflater.inflate(R.layout.app_title, parent, false)
+            )
+            SPACER_SECTION_TYPE -> SectionViewHolder.StaticSectionViewHolder(
+                layoutInflater.inflate(R.layout.bottom_sections_spacer, parent, false)
+            )
+            else -> SectionViewHolder.MediaSectionViewHolder(
+                layoutInflater.inflate(
+                    R.layout.section,
+                    parent,
+                    false
+                )
+            ).apply {
+                recyclerView.apply {
+                    adapter = MediaItemsAdapter(activity, placeholder, error)
+                    layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+                }
+            }
+        }
+
+    override fun getItemCount() = 2 + itemsProviders.size
+
+    override fun getItemId(position: Int): Long = when (position) {
+        0 -> APP_SECTION_TYPE.toLong()
+        itemsProviders.size + 1 -> SPACER_SECTION_TYPE.toLong()
+        else -> itemsProviders[position - 1].type.ordinal.toLong()
+    }
+
+    override fun onBindViewHolder(holder: SectionViewHolder, position: Int) {
+        if (holder is SectionViewHolder.MediaSectionViewHolder) {
+            itemsProviders[position - 1].run {
+                holder.title.text = when (this.type) {
+                    ProviderType.Trending -> activity.getText(R.string.trending_list_title)
+                    ProviderType.Favorites -> activity.getText(R.string.favorites_list_title)
+                    ProviderType.History -> activity.getText(R.string.history_list_title)
+                    ProviderType.Search -> activity.getText(R.string.search_results_title)
+                }
+
+                this.setOnItemsAvailableListener((holder.recyclerView.adapter as MediaItemsAdapter)::setItems)
+            }
+        }
+    }
+
+    fun setItemsProviders(providers: List<ItemsProvider>) {
+        itemsProviders = providers
+        notifyDataSetChanged()
+    }
+
+    companion object {
+        const val APP_SECTION_TYPE = 0
+        const val SPACER_SECTION_TYPE = 1
+        const val MEDIA_SECTION_TYPE = 3
+    }
+}
+
+sealed class SectionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class StaticSectionViewHolder(view: View) : SectionViewHolder(view)
+    class MediaSectionViewHolder(view: View) : SectionViewHolder(view) {
+        var title: TextView = view.findViewById(R.id.section_title)
+        var recyclerView: RecyclerView = view.findViewById(R.id.section_list)
+    }
+}
+
+private class MediaItemsAdapter(
+    private val activity: Activity,
+    private val placeholder: Drawable, private val error: Drawable
+) :
     RecyclerView.Adapter<MediaItemViewHolder>() {
     private var viewTarget: Target<*>? = null
 
     private val layoutInflater = LayoutInflater.from(activity)
     private var items = emptyList<Media>()
 
+    init {
+        setHasStableIds(true)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         MediaItemViewHolder(layoutInflater.inflate(R.layout.media_item, parent, false))
 
     override fun getItemCount() = items.count()
 
+    override fun getItemId(position: Int): Long = items[position].original.hashCode().toLong()
+
     fun setItems(newItems: List<Media>) {
+        newItems.forEach { Log.d("MediaItemsAdapter", "item: ${it.original}") }
         items = newItems
         notifyDataSetChanged()
     }
@@ -130,7 +201,7 @@ private class MediaItemsAdapter(private val activity: Activity, private val plac
             Glide.with(activity)
                 .load(it.preview)
                 .placeholder(placeholder)
-                .error(R.drawable.ic_error_loading)
+                .error(error)
                 .into(holder.image)
         }
     }
--- app/src/main/java/net/evendanan/lumiere/presenter.kt
+++ app/src/main/java/net/evendanan/lumiere/presenter.kt
@@ -12,21 +12,38 @@ interface Presenter {
     fun destroy()
 }
 
+enum class ProviderType {
+    Trending,
+    Favorites,
+    History,
+    Search
+}
+
+interface ItemsProvider {
+    val type: ProviderType
+    fun setOnItemsAvailableListener(listener: (List<Media>) -> Unit)
+}
+
 interface PresenterUI {
     fun setQueryBoxVisibility(visible: Boolean)
     fun getQueryBoxText(): String
-    fun setSearchResults(entries: List<Media>)
-    fun setHistory(entries: List<Media>)
-    fun setFavorites(entries: List<Media>)
-    fun setTrending(entries: List<Media>)
+    fun setItemsProviders(providers: List<ItemsProvider>)
+    fun focusOnSection(providerType: ProviderType)
 }
 
 class PresenterImpl(private val mediaProvider: MediaProvider, private val ui: PresenterUI) : Presenter {
+
+    private val availableProviders =
+        mutableMapOf(ProviderType.Trending to ItemsProviderImpl(ProviderType.Trending))
 
     private var viewModelJob = Job()
     private var searchJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main)
     private var inSearchState = false
+
+    init {
+        ui.setItemsProviders(availableProviders.values.toSortedCollection())
+    }
 
     override fun onUiVisible() {
         ui.setQueryBoxVisibility(inSearchState)
@@ -36,8 +53,7 @@ class PresenterImpl(private val mediaProvider: MediaProvider, private val ui: Pr
             val trending = withContext(Dispatchers.Default) {
                 mediaProvider.blockingTrending()
             }
-
-            ui.setTrending(trending)
+            availableProviders[ProviderType.Trending]?.setItems(trending)
         }
     }
 
@@ -54,7 +70,14 @@ class PresenterImpl(private val mediaProvider: MediaProvider, private val ui: Pr
     override fun onFabClicked() {
         if (inSearchState) {
             searchJob.cancel()
-            ui.setSearchResults(emptyList())
+
+            if (!availableProviders.containsKey(ProviderType.Search)) {
+                availableProviders[ProviderType.Search] = ItemsProviderImpl(ProviderType.Search)
+                ui.setItemsProviders(availableProviders.values.toSortedCollection())
+            }
+
+            //consider putting a local-loading GIF image here.
+            availableProviders[ProviderType.Search]?.setItems(emptyList())
             searchJob = uiScope.launch(Dispatchers.Main.immediate) {
                 val search = ui.getQueryBoxText().let { queryString ->
                     if (queryString.isEmpty()) {
@@ -65,7 +88,8 @@ class PresenterImpl(private val mediaProvider: MediaProvider, private val ui: Pr
                         }
                     }
                 }
-                ui.setSearchResults(search)
+                availableProviders[ProviderType.Search]?.setItems(search)
+                ui.focusOnSection(ProviderType.Search)
             }
         } else {
             inSearchState = true
@@ -73,3 +97,31 @@ class PresenterImpl(private val mediaProvider: MediaProvider, private val ui: Pr
         }
     }
 }
+
+private class ItemsProviderImpl(override val type: ProviderType) : ItemsProvider {
+    private var items = emptyList<Media>()
+    private var listener: ((List<Media>) -> Unit) = this::noOpListener
+
+    override fun setOnItemsAvailableListener(listener: (List<Media>) -> Unit) {
+        this.listener = listener
+        notifyItems()
+    }
+
+    fun setItems(items: List<Media>) {
+        this.items = items
+        notifyItems()
+    }
+
+    private fun notifyItems() {
+        listener(items)
+    }
+
+    fun noOpListener(items: List<Media>) {}
+}
+
+private fun Collection<ItemsProvider>.toSortedCollection() =
+    this.toMutableList().apply {
+        sortWith(Comparator { o1, o2 ->
+            o1.type.ordinal - o2.type.ordinal
+        })
+    }
