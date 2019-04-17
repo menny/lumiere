bd6aa650486683d06182ae149a722a9a27d29664
Menny Even Danan <menny@evendanan.net>
2019-04-16 22:48:19 -0400

    bootstraping release deploy assets

--- scripts/ci/ci_check.sh
+++ scripts/ci/ci_check.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 ./gradlew ${EXTRA_GRADLE_ARGS} lintDebug
-./gradlew ${EXTRA_GRADLE_ARGS} verifyReleaseResources
-#./gradlew ${EXTRA_GRADLE_ARGS} generateReleasePlayResources
+./gradlew ${EXTRA_GRADLE_ARGS} generateReleasePlayResources
